c3d8eca9f6a85b8b22b74cf1e2aabee48802d4dc
matveyadamey <71337113+matveyadamey@users.noreply.github.com>
2024-02-04 12:27:42 +0300

    ра папа

--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -14,18 +14,12 @@ namespace UnityEngine.AzureSky
         {
             var pythonArgs = "C:/Users/matve/Documents/GitHub/Cube-Game4.0/Assets/Scripts/getAll.py";
             System.Diagnostics.Process.Start("python", pythonArgs);
-            string file1 = @"weather.txt";
-            if (File.Exists(file1))
-            {
-                weatherState = int.Parse(File.ReadAllText(file1));
-            }
-            string file2 = @"time.txt";
-            if (File.Exists(file2))
-            {
-                time = int.Parse(File.ReadAllText(file2));
-            }
+            string file1 = @"Assets/Scripts/weather.txt";
+            weatherState = int.Parse(File.ReadAllText(file1));
+            Azure.SetNewWeatherProfile(weatherState);
+            string file2 = @"Assets/Scripts/time.txt";
+            time = int.Parse(File.ReadAllText(file2));
             Time.SetTimeline(time);
-            Azure.SetNewWeatherProfile(6);
         }
     }
 }
