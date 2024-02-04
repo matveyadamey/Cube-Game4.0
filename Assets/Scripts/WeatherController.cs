bee1dd3b8dedbd0e05cd29223c01bcdd387598ba
matveyadamey <71337113+matveyadamey@users.noreply.github.com>
2024-02-04 11:54:31 +0300

    погода готова блин наконец-то

--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -1,36 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 namespace UnityEngine.AzureSky
 {
     public class WeatherController : MonoBehaviour
     {
         public AzureWeatherController Azure;
-        // Start is called before the first frame update
-        void Start()
+        public AzureTimeController Time;
+        int time;
+        int weatherState;
+        private void Start()
         {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetButton("h"))
+            var pythonArgs = "C:/Users/matve/Documents/GitHub/Cube-Game4.0/Assets/Scripts/getAll.py";
+            System.Diagnostics.Process.Start("python", pythonArgs);
+            string file1 = @"weather.txt";
+            if (File.Exists(file1))
             {
-                Azure.SetNewWeatherProfile(6);
+                weatherState = int.Parse(File.ReadAllText(file1));
             }
-            if (Input.GetButton("j"))
+            string file2 = @"time.txt";
+            if (File.Exists(file2))
             {
-                Azure.SetNewWeatherProfile(2);
+                time = int.Parse(File.ReadAllText(file2));
             }
-            if (Input.GetButton("k"))
-            {
-                Azure.SetNewWeatherProfile(7);
-            }
-            if (Input.GetButton("l"))
-            {
-                Azure.SetNewWeatherProfile(0);
-            }
+            Time.SetTimeline(time);
+            Azure.SetNewWeatherProfile(6);
         }
     }
 }
