57723437ea47942531fd8bb54c17ce48eae4fe7e
matveyadamey <71337113+matveyadamey@users.noreply.github.com>
2024-02-04 12:03:07 +0300

    Revert "погода готова блин наконец-то"
    This reverts commit bee1dd3b8dedbd0e05cd29223c01bcdd387598ba.

--- Assets/Scripts/WeatherController.cs
+++ Assets/Scripts/WeatherController.cs
@@ -1,31 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 namespace UnityEngine.AzureSky
 {
     public class WeatherController : MonoBehaviour
     {
         public AzureWeatherController Azure;
-        public AzureTimeController Time;
-        int time;
-        int weatherState;
-        private void Start()
+        // Start is called before the first frame update
+        void Start()
         {
-            var pythonArgs = "C:/Users/matve/Documents/GitHub/Cube-Game4.0/Assets/Scripts/getAll.py";
-            System.Diagnostics.Process.Start("python", pythonArgs);
-            string file1 = @"weather.txt";
-            if (File.Exists(file1))
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButton("h"))
             {
-                weatherState = int.Parse(File.ReadAllText(file1));
+                Azure.SetNewWeatherProfile(6);
             }
-            string file2 = @"time.txt";
-            if (File.Exists(file2))
+            if (Input.GetButton("j"))
             {
-                time = int.Parse(File.ReadAllText(file2));
+                Azure.SetNewWeatherProfile(2);
             }
-            Time.SetTimeline(time);
-            Azure.SetNewWeatherProfile(6);
+            if (Input.GetButton("k"))
+            {
+                Azure.SetNewWeatherProfile(7);
+            }
+            if (Input.GetButton("l"))
+            {
+                Azure.SetNewWeatherProfile(0);
+            }
         }
     }
 }
